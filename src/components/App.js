2f2fbff75389ff973dcae7699ca9b88c05287ea2
ameriken <hitohahito@hotmail.co.jp>
2020-06-24 19:54:39 +0900

    イベント一覧に表示させる

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,53 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Event from './Event'
+import reducer from '../reducers/index'
 
 const App = () => {
-  return (
-    <div className="container-fluid">
-        <h4>イベント作成フォーム</h4>
-        <form>
-            <div className="form-group">
-                <label htmlFor="formEventTitle">タイトル</label>
-                <input className="form-control" id="formEventTitle" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="formEventBody">ボディー</label>
-                <textarea className="form-control" id="formEventBody" />
-            </div>
-            <button className="btn btn-primary">イベントを作成する</button>
-            <button className="btn btn-danger">イベントを削除する</button>
-        </form>
-        <h4>イベント一覧</h4>
+    const [state, dispatch] = useReducer(reducer, [])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
-        <table className="table table-hover">
-            <thead>
+    const addEvent = e => {
+        e.preventDefault()
+        dispatch({
+            type: 'CREATE_EVENT',
+            title,
+            body
+        })
+    }
 
-                <tr>
-                    <th>ID</th>
-                    <th>タイトル</th>
-                    <th>ボディー</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <h4>イベント作成フォーム</h4>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="formEventTitle">タイトル</label>
+                    <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="formEventBody">ボディー</label>
+                    <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
+                </div>
+                <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
+                <button className="btn btn-danger">イベントを削除する</button>
+            </form>
+            <h4>イベント一覧</h4>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>タイトル</th>
+                        <th>ボディー</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
