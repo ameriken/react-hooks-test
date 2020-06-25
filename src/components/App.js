782f61a97cf2c21ed15aa7930591a713591b622d
ameriken <hitohahito@hotmail.co.jp>
2020-06-26 03:01:25 +0900

    localstrageで永続化

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,21 @@ import EventForm from './EventForm'
 import Events from './Events'
 import reducer from '../reducers/index'
 import AppContext from "../contexts/AppContext";
-import OperationLogs from "../components/operationLogs";
+import OperationLogs from "../components/OperationLogs";
+
+const APP_KEY = 'appWithRedux'
 
 
 const App = () => {
-    const initialState = {
+    const appState = localStorage.getItem(APP_KEY)
+    const initialState =  appState ? JSON.parse(appState) : {
         events: [],
         operationLogs: []
     }
     const [state, dispatch] = useReducer(reducer, initialState)
-
+    useEffect(() => {
+        localStorage.setItem(APP_KEY, JSON.stringify(state))
+    }, [state])
 
     return (
         <AppContext.Provider value={{state, dispatch}}>
